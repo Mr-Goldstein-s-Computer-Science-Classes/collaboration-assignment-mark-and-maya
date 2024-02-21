0af96e67b8e8d3d4ddd075b1cf1a79458ecd683b
markgelfand <113187474+markgelfand@users.noreply.github.com>
2024-02-21 12:53:18 -0500

    student class (not finished)

--- Student.java
+++ Student.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
 public class Student {
+
+    private String name;
+    private ArrayList<String> assignments;
+    private double grade;
+
+
+    public Student(String name) {
+        this.name = name;
+
+    }
+
+    public void addAssignment(String assignName) {
+        assignments.add(assignName);
+    }
+
+    public void gradeAssignment(String assignment, double grade) {
+        String result = assignment + "grade: " + grade;
+        assignments.remove(assignment);
+        assignments.add(result);
+    }
+
+    double getAssignmentGrade(String assignment) {
+        return grade;
+    }
+
+    public double getOverallGrade() {
+    return
+    }
+
+    public String toString() {
+    return "g";
+    }
+
 
 }
